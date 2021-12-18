35930050a184d0b196f7c679c2b8f34e8bd32027
hinatakahashi <hina0414takahashi@gmail.com>
2021-12-18 16:20:32 +0900

    テーブル設計＆作成

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,59 +1,50 @@
+DROP SCHEMA IF EXISTS webapp; 
+CREATE SCHEMA webapp;
+USE webapp;
+
 -- 大元のテーブル
+
+-- 日付, 学習言語, 学習コンテンツ, 学習時間
 DROP TABLE IF EXISTS `study`;
 CREATE TABLE `study`(
   `id` INT NOT NULL PRIMARY KEY,
-  `date` DATE DEFAULT NULL,
-  `content` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `language` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `time` FLOAT(2,2) DEFAULT NULL,
-  `comment` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+  `date` DATE NOT NULL,
+  `language` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `content` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `time` INT NOT NULL,
 );
 
--- INSERT INTO `study` (`id`,`date`,`content`,`language`,`time`,`comment`) VALUES
--- (1,'東京の難読地名クイズ');
+INSERT INTO `study` (`id`,`date`,`language`,`content`,`time`) VALUES
+(2021-12-18,'N予備校','PHP',8);
 
 -- 細かいテーブル
-DROP TABLE IF EXISTS `study_date`;
-CREATE TABLE `study_date` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `date` DATE　DEFAULT NULL
-  );
 
-INSERT INTO `choices` (`id`,`date`) VALUES
-(1,1,'たかなわ',1);
-
-DROP TABLE IF EXISTS ``;
-CREATE TABLE `study_content` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `content` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
-  );
-
-INSERT INTO `study_content` (`id`,`content`) VALUES
-(1,1,'たかなわ',1);
-
+-- 学習言語
 DROP TABLE IF EXISTS `study_language`;
 CREATE TABLE `study_language` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `language` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+  `id` INT NOT NULL PRIMARY KEY,
+  `language` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `language_color` VARCHAR(50) NOT NULL
   );
 
-INSERT INTO `choices` (`id`,`language`) VALUES
-(1,1,'たかなわ',1);
+INSERT INTO `study_language` (`language`,`language_color`) VALUES ('JavaScript','1754ef');
+INSERT INTO `study_language` (`language`,`language_color`) VALUES ('CSS','2171bd');
+INSERT INTO `study_language` (`language`,`language_color`) VALUES ('PHP','4dbdde');
+INSERT INTO `study_language` (`language`,`language_color`) VALUES ('HTML','54cef9');
+INSERT INTO `study_language` (`language`,`language_color`) VALUES ('Laravel','b29ef3');
+INSERT INTO `study_language` (`language`,`language_color`) VALUES ('SQL','6d46ec');
+INSERT INTO `study_language` (`language`,`language_color`) VALUES ('SHELL','4f45ef');
+INSERT INTO `study_language` (`language`,`language_color`) VALUES ('情報システム基礎知識(その他)','3636c1');
 
-DROP TABLE IF EXISTS `study_time`;
-CREATE TABLE `study_time` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `time` FLOAT(2,2) DEFAULT NULL
+
+-- 学習コンテンツ
+DROP TABLE IF EXISTS `study_contents`;
+CREATE TABLE `study_contents` (
+  `id` INT NOT NULL PRIMARY KEY,
+  `content` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `content_color` VARCHAR(50) NOT NULL
   );
 
-INSERT INTO `choices` (`id`,`time`) VALUES
-(1,1,'たかなわ',1);
-
-DROP TABLE IF EXISTS `study_comment`;
-CREATE TABLE `study_comment` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `date` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
-  );
-
-INSERT INTO `choices` (`id`,`comment`) VALUES
-(1,1,'たかなわ',1);
+INSERT INTO `study_contents` (`content`,`content_color`) VALUES ('ドットインストール','1754ef');
+INSERT INTO `study_contents` (`content`,`content_color`) VALUES ('N予備校','2171bd');
+INSERT INTO `study_contents` (`content`,`content_color`) VALUES ('POSSE課題','4dbdde');
--- src/index.php
+++ src/index.php
@@ -1,68 +1,182 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . "/db_connect.php");
-
-$number = $_GET['id'];
-
-$stmt = "SELECT * FROM questions WHERE $number = questions.id";
-$questions = $db->query($stmt)->fetchAll();
-
-
-$stmt = "SELECT name FROM choices WHERE $number = choices.question_id";
-$choices = $db->query($stmt)->fetchAll(PDO::FETCH_ASSOC); //カラム名だけひっぱる
-// print_r($choices);
-// exit;
-
-// print_r($choices[0]['name']);
-// print_r($choices[1]['name']);
-// print_r($choices[2]['name']);
-// exit;
-
-foreach ($questions as $question) {
-
-  // print_r ($question['name']);
-  
-}
-
-// foreach ($choices as $choice) {
-
-//   print_r ($choice['name']);
-  
-// }
-// foreach ($choices as $choice) {
-
-  // var_dump ($choices[1][0]);
-  
-// }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ガチで東京の人しか解けない！#東京の難読地名クイズ</title>
-    <link rel="stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ぽっしょ</title>
+	<!-- ↓jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<!-- 　↓chart.js↓ -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+	<!-- datalabelsプラグインを呼び出す -->
+	<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="modal.css">
 </head>
+
 <body>
-    <div class="main">
+	<!-- ヘッダー -->
+	<header>
+		<h1 class="header">
+			<div class="header_div1">
+				<img src="./img/posselogo.jpg" alt="posseロゴ" class="posselogo">
+				<!-- <button type="menu" class="button">記録・投稿</button> -->
+				<span class="nthweek">4th week</span>
+			</div>
+			<div id="open">
+				記録・投稿
+			</div>
 
-        <div class="quiz">
-            <h1><?php print_r ($question['name']); ?></h1>
-            <img src="img1.png">
-            <ul>
-                <li id="answerlist_1_1" name="answerlist_1" class="answerlist" onclick="check(1, 1, 1)"><?php print_r($choices[0]['name']);?></li>
-                <li id="answerlist_1_2" name="answerlist_1" class="answerlist" onclick="check(1, 2, 1)"><?php print_r($choices[1]['name']);?></li>
-                <li id="answerlist_1_3" name="answerlist_1" class="answerlist" onclick="check(1, 3, 1)"><?php print_r($choices[2]['name']);?></li>
-                <li id="answerbox_1" class="answerbox">
-                    <span id="answertext_1"></span><br>
-                    <span>正解は「<?php print_r($choices[0]['name']);?>」です！</span>
-                </li>
-            </ul>
-        </div>
+			<div id="mask" class="hidden"></div>
 
-        <script src="./quizy1.js"></script>
-    </div>
+			<!-- モーダル -->
+			<div id="modal" class="hidden">
+				<div id="close">
+					<span class="dli-close"></span>
+				</div>
+				<div class="modalcontainer">
+					<div class="modalleft">
+						<div class="modaldate">
+							<h2>学習日</h2>
+							<p class="gakushubi"><input type="text" id="date"></p>
+							<div id="calendar"></div>
+						</div>
+						<div class="modalcontents">
+							<h2>学習コンテンツ(複数選択可)</h2>
+							<form action="gakushucontents" method="get">
+								<input type="checkbox" name="contents" value="1"
+									id="nyobi">N予備校
+								<input type="checkbox" name="contents" value="2"
+									id="dotinstall">ドットインストール<br>
+								<input type="checkbox" name="contents" value="3"
+									id="possekadai">POSSE課題
+							</form>
+						</div>
+						<div class="modallanguages">
+							<h2>学習言語(複数選択可)</h2>
+							<form action="gakushugengo" method="get">
+								<input type="checkbox" name="languages" value="1"
+									id="HTML">HTML
+								<input type="checkbox" name="languages" value="2"
+									id="CSS">CSS
+								<input type="checkbox" name="languages" value="3"
+									id="Javascript">Javascript<br>
+								<input type="checkbox" name="languages" value="4"
+									id="PHP">PHP
+								<input type="checkbox" name="languages" value="5"
+									id="Laravel">Laravel
+								<input type="checkbox" name="languages" value="6"
+									id="SQL">SQL
+								<input type="checkbox" name="languages" value="7"
+									id="SHELL">SHELL<br>
+								<input type="checkbox" name="languages" value="8"
+									id="情報システム基礎知識(その他)">情報システム基礎知識(その他)
+							</form>
+						</div>
+						　
+					</div>
+					<div class="modalright">
+						<div class="length">
+							<h2>学習時間</h2>
+							<p><input type="text" id="time"></p>
+						</div>
+						<div class="comments">
+							<h2>Twitter用コメント</h2>
+							<p><input type="text" id="comment"></p>
+						</div>
+						<div class="twitterbtn">
+							<input type="checkbox" name="postontwitter"
+								class="postontwitter">
+							<span>Twitterに自動投稿する</span>
+						</div>
+					</div>
+				</div>
+				<div class="buttonposition">
+					<button type="menu" class="button">記録・投稿</button>
+				</div>
+			</div>
+			<div id="success" class="hidden">
+				<img src="./success.png" class="successpng" alt="完了画面">
+			</div>
+
+		</h1>
+	</header>
+	<main>
+		<div class="container">
+			<div class="left">
+				<div class="items">
+					<li class="studytimeoftoday">
+						<p class="whentitle">Today</p>
+						<p class="number">3</p>
+						<p class="hour">hour</p>
+					</li>
+					<li class="studytimeofmonth">
+						<p class="whentitle">Month</p>
+						<p class="number">20</p>
+						<p class="hour">hour</p>
+					</li>
+					<li class="studytimeoftotal">
+						<p class="whentitle">Total</p>
+						<p class="number">1213</p>
+						<p class="hour">hour</p>
+					</li>
+				</div>
+				<!-- グラフ -->
+				<div class="container_bargraph">
+					<canvas id="bargraph"></canvas>
+				</div>
+			</div>
+			<div class="right">
+				<!-- 学習言語グラフ -->
+				<div class="languages">
+					<div class="gakushugengo">
+						<h2>学習言語</h2>
+					</div>
+					<div class="container_piechart1">
+						<canvas id="piechart1"></canvas>
+					</div>
+					<div>
+						<ul>
+							<li class="languages1">Javascript</li>
+							<li class="languages2">CSS</li>
+							<li class="languages3">PHP</li><br>
+							<li class="languages4">HTML</li>
+							<li class="languages5">Laravel</li>
+							<li class="languages6">SQL</li><br>
+							<li class="languages7">SHELL</li><br>
+							<li class="languages8">情報システム基礎知識(その他)</li>
+						</ul>
+					</div>
+				</div>
+				<!-- 学習コンテンツグラフ -->
+				<div class="contents">
+					<div class="gakushucontents">
+						<h2>学習コンテンツ</h2>
+						<div class="container_piechart2">
+							<canvas id="piechart2"></canvas>
+						</div>
+						<ul>
+							<li class="contents1">ドットインストール</li>
+							<li class="contents2">N予備校</li><br>
+							<li class="contents3">POSSE課題</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer>
+		<div class="footer">
+			<h2> ＜ 2010年　10月 ＞ </h2>
+		</div>
+	</footer>
+	<script src="./index.js"></script>
 </body>
 
 </html>

c0fd75175b72c067260c9eb693908a2c031ba487
hinatakahashi <hina0414takahashi@gmail.com>
2021-12-22 21:44:03 +0900

    変数名,インデントの修正 テーブルとかラムにコメント追加

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -15,18 +15,36 @@ CREATE TABLE `study`(
   `time` INT NOT NULL,
 );
 
+/
+COMMENT ON TABLE study IS '学習時間合計テーブル'
+/
+COMMENT ON COLUMN date IS '学習した日付'
+/
+COMMENT ON COLUMN language IS '学習言語'
+/
+COMMENT ON COLUMN language IS '学習コンテンツ'
+/
+COMMENT ON COLUMN language IS '学習時間'
+/
+
 INSERT INTO `study` (`id`,`date`,`language`,`content`,`time`) VALUES
 (2021-12-18,'N予備校','PHP',8);
 
--- 細かいテーブル
 
--- 学習言語
 DROP TABLE IF EXISTS `study_language`;
 CREATE TABLE `study_language` (
   `id` INT NOT NULL PRIMARY KEY,
   `language` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
   `language_color` VARCHAR(50) NOT NULL
   );
+
+/
+COMMENT ON TABLE study IS '学習言語テーブル'
+/
+COMMENT ON COLUMN language IS '学習言語'
+/
+COMMENT ON COLUMN language_color IS '学習言語ごとの色'
+/
 
 INSERT INTO `study_language` (`language`,`language_color`) VALUES ('JavaScript','1754ef');
 INSERT INTO `study_language` (`language`,`language_color`) VALUES ('CSS','2171bd');
@@ -38,13 +56,20 @@ INSERT INTO `study_language` (`language`,`language_color`) VALUES ('SHELL','4f45
 INSERT INTO `study_language` (`language`,`language_color`) VALUES ('情報システム基礎知識(その他)','3636c1');
 
 
--- 学習コンテンツ
 DROP TABLE IF EXISTS `study_contents`;
 CREATE TABLE `study_contents` (
   `id` INT NOT NULL PRIMARY KEY,
   `content` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
   `content_color` VARCHAR(50) NOT NULL
   );
+
+/
+COMMENT ON TABLE study IS '学習コンテンツテーブル'
+/
+COMMENT ON COLUMN language IS '学習コンテンツ'
+/
+COMMENT ON COLUMN language_color IS '学習コンテンツごとの色'
+/
 
 INSERT INTO `study_contents` (`content`,`content_color`) VALUES ('ドットインストール','1754ef');
 INSERT INTO `study_contents` (`content`,`content_color`) VALUES ('N予備校','2171bd');
--- src/index.js
+++ src/index.js
@@ -1,93 +1,93 @@
 'use strict';
-//↓棒グラフ
-var ctx = document.getElementById("bargraph").getContext("2d");
-    var myBar = new Chart(ctx, {
-        type: 'bar',                           //◆棒グラフ
-        // lineWidth: 100,
-        data: {                                //◆データ
-            labels: ['2','4','6','8','10','12','14','16','18','20','22','24','26','28','30'],     //ラベル名
-            datasets: [{                       //データ設定
-                data: [2,3,4,4,1,5,6,5,4,4,3,2,3,7,4],          //データ内容
-                backgroundColor: '#2aaae4'   //背景色
-            },
-            {
-                data: [2,3,6,7,8,3,2,4,5,4,4,3,4,8,2],
-                backgroundColor: '#2aaae4'
-            },
-          ],
+//学習時間棒グラフ
+var context = document.getElementById("studytime_bargraph").getContext("2d");
+var data_information = new Chart(context, {
+  type: 'bar',                           //◆棒グラフ
+  // lineWidth: 100,
+  data: {                                //◆データ
+    labels: ['2', '4', '6', '8', '10', '12', '14', '16', '18', '20', '22', '24', '26', '28', '30'],     //ラベル名
+    datasets: [{                       //データ設定
+      data: [2, 3, 4, 4, 1, 5, 6, 5, 4, 4, 3, 2, 3, 7, 4],          //データ内容
+      backgroundColor: '#2aaae4'   //背景色
+    },
+    {
+      data: [2, 3, 6, 7, 8, 3, 2, 4, 5, 4, 4, 3, 4, 8, 2],
+      backgroundColor: '#2aaae4'
+    },
+    ],
+  },
+  options: {                             //◆オプション
+    responsive: true,                  //グラフ自動設定
+    legend: {                          //凡例設定
+      display: false                 //表示設定
+    },
+    // title: {                           //タイトル設定
+    //     display: true,                 //表示設定
+    //     fontSize: 18,                  //フォントサイズ
+    //     text: 'タイトル'                //ラベル
+    // },
+    scales: {                          //軸設定
+      yAxes: [{                      //y軸設定
+        display: true,             //表示設定
+        scaleLabel: {              //軸ラベル設定
+          display: true,          //表示設定
+          //  labelString: '縦軸ラベル',  //ラベル
+          //  fontSize: 18               //フォントサイズ
         },
-        options: {                             //◆オプション
-            responsive: true,                  //グラフ自動設定
-            legend: {                          //凡例設定
-                display: false                 //表示設定
-          },
-            // title: {                           //タイトル設定
-            //     display: true,                 //表示設定
-            //     fontSize: 18,                  //フォントサイズ
-            //     text: 'タイトル'                //ラベル
-            // },
-            scales: {                          //軸設定
-                yAxes: [{                      //y軸設定
-                    display: true,             //表示設定
-                    scaleLabel: {              //軸ラベル設定
-                       display: true,          //表示設定
-                      //  labelString: '縦軸ラベル',  //ラベル
-                      //  fontSize: 18               //フォントサイズ
-                    },
-                    ticks: {                      //最大値最小値設定
-                        min: 0,                   //最小値
-                        max: 8,                  //最大値
-                        // fontSize: 18,             //フォントサイズ
-                        stepSize: 2,               //軸間隔
-                    },
-                }],
-                xAxes: [{                         //x軸設定
-                  　
-                    display: true,                //表示設定
-                    scaleLabel: {
-                      display: true,
-                    },
-                    barPercentage: 0.5,           //棒グラフ幅
-                    categoryPercentage: 0.8,      //棒グラフ幅
-                    // scaleLabel: {                 //軸ラベル設定
-                    //    display: true,             //表示設定
-                      //  labelString: '横軸ラベル',  //ラベル
-                    //   //  fontSize: 18               //フォントサイズ
-                    // },
-                    ticks: {
-                      min: 2,
-                      max: 30,
-                      stepSize: 2
-                        // fontSize: 18             //フォントサイズ
-                    },
-                }],
-            },
+        ticks: {                      //最大値最小値設定
+          min: 0,                   //最小値
+          max: 8,                  //最大値
+          // fontSize: 18,             //フォントサイズ
+          stepSize: 2,               //軸間隔
         },
-      });
+      }],
+      xAxes: [{                         //x軸設定
+
+        display: true,                //表示設定
+        scaleLabel: {
+          display: true,
+        },
+        barPercentage: 0.5,           //棒グラフ幅
+        categoryPercentage: 0.8,      //棒グラフ幅
+        // scaleLabel: {                 //軸ラベル設定
+        //    display: true,             //表示設定
+        //  labelString: '横軸ラベル',  //ラベル
+        //   //  fontSize: 18               //フォントサイズ
+        // },
+        ticks: {
+          min: 2,
+          max: 30,
+          stepSize: 2
+          // fontSize: 18             //フォントサイズ
+        },
+      }],
+    },
+  },
+});
 
 //↓円グラフ１
-var ctx1 = document.getElementById("piechart1");
-var piechart1= new Chart(ctx1, {
+var language_data = document.getElementById("piechart1");
+var piechart1 = new Chart(language_data, {
   type: 'doughnut',
   data: {
     labels: { //データ項目のラベル
       display: false
     },
     datasets: [{
-        backgroundColor: [
-            "#1754ef",
-            "#2171bd",
-            "#4dbdde",
-            "#54cef9",
-            "#b29ef3",
-            "#6d46ec",
-            "#4f45ef",
-            "#3636c1",
-            "#1754ef",
-            "#2171bd",
-            "#4dbdde"
-        ],
-        data: [42,18,10,8,7,5,3,2] //グラフのデータ
+      backgroundColor: [
+        "#1754ef",
+        "#2171bd",
+        "#4dbdde",
+        "#54cef9",
+        "#b29ef3",
+        "#6d46ec",
+        "#4f45ef",
+        "#3636c1",
+        "#1754ef",
+        "#2171bd",
+        "#4dbdde"
+      ],
+      data: [42, 18, 10, 8, 7, 5, 3, 2] //グラフのデータ
     }]
   },
   options: {
@@ -97,14 +97,14 @@ var piechart1= new Chart(ctx1, {
       responsive: false,
       plugins: {
         datalabels: {
-            color: '#000',
-            font: {
-                weight: 'bold',
-                size: 20,
-            },
-            formatter: (value, ctx1) => {
-                return value + '%';
-            },
+          color: '#000',
+          font: {
+            weight: 'bold',
+            size: 20,
+          },
+          formatter: (value, language_data) => {
+            return value + '%';
+          },
         }
       }
     }
@@ -112,20 +112,20 @@ var piechart1= new Chart(ctx1, {
 });
 
 //↓円グラフ２
-var ctx2 = document.getElementById("piechart2");
-var piechart2= new Chart(ctx2, {
+var content_data = document.getElementById("piechart2");
+var piechart2 = new Chart(content_data, {
   type: 'doughnut',
   data: {
     labels: { //データ項目のラベル 
-      display: false 
+      display: false
     },
     datasets: [{
-        backgroundColor: [
-            "#1754ef",
-            "#2171bd",
-            "#4dbdde",
-        ],
-        data: [42,33,25] //グラフのデータ
+      backgroundColor: [
+        "#1754ef",
+        "#2171bd",
+        "#4dbdde",
+      ],
+      data: [42, 33, 25] //グラフのデータ
     }]
   },
   options: {
@@ -138,7 +138,7 @@ var piechart2= new Chart(ctx2, {
 
 
 // モーダル
-(function() {
+(function () {
   'use strict';
 
   var open = document.getElementById('open');
@@ -147,43 +147,35 @@ var piechart2= new Chart(ctx2, {
   var mask = document.getElementById('mask');
   // var success = document.getElementById('success');
 
-  open.addEventListener('click', function() {
+  open.addEventListener('click', function () {
     modal.className = '';
     mask.className = '';
   });
 
-  close.addEventListener('click', function() {
+  close.addEventListener('click', function () {
     modal.className = 'hidden';
     mask.className = 'hidden';
-    // success.className = 'hidden';
-    // loading.className = 'hidden';
   });
 
-  mask.addEventListener('click', function() {
-    // modal.className = 'hidden';
-    // mask.className = 'hidden';
-    // success.className = 'hidden';
+  mask.addEventListener('click', function () {
+
     close.click();
   });
 })();
 
-// カレンダー表示
 $(function () {
+  // カレンダー表示
   $('.gakushubi').on('click', () => {
-      $('#calendar').toggle(); //toggle←要素を表示なら非表示に、非表示なら表示に切り替える
+    $('#calendar').toggle(); //toggle←要素を表示なら非表示に、非表示なら表示に切り替える
   });
-});
-
-//ローディング画面
-$(function () {
+  //ローディング画面
   $('.buttonposition').on('click', () => {
-      $('#success').show(); 
-      $('#modal').hide(); 
+    $('#success').show();
+    $('#modal').hide();
+  });
+  $('#mask').on('click', () => {
+    $('#success').hide();
   });
 });
 
-$(function () {
-  $('#mask').on('click', () => {
-      $('#success').hide();
-  });
-});
+

db3b8725373220f22c404ca62445eeab0f777bb2
hinatakahashi <hina0414takahashi@gmail.com>
2022-03-24 23:19:34 +0900

    データ挿入

--- template-3rd-study/database/seeds/ContentTableSeeder.php
+++ template-3rd-study/database/seeds/ContentTableSeeder.php
@@ -11,6 +11,23 @@ class ContentTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+    //データのクリア
+    DB::table('contents')->truncate();
+    $param =[
+        [
+            'study_content'=>'N予備校',
+            'content_color'=>'1754ef'
+        ],
+        [
+            'study_content'=>'ドットインストール',
+            'content_color'=>'2171bd'
+        ],
+        [
+            'study_content'=>'POSSE課題',
+            'content_color'=>'4dbdde'
+        ]
+        
+    ];
+    DB::table('contents')->insert($param);
     }
 }
--- template-3rd-study/database/seeds/LanguageTableSeeder.php
+++ template-3rd-study/database/seeds/LanguageTableSeeder.php
@@ -11,6 +11,43 @@ class LanguageTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+    //データのクリア
+    DB::table('languages')->truncate();
+    $param =[
+        [
+            'study_language'=>'JavaScript',
+            'language_color'=>'1754ef'
+        ],
+        [
+            'study_language'=>'CSS',
+            'language_color'=>'2171bd'
+        ],
+        [
+            'study_language'=>'PHP',
+            'language_color'=>'4dbdde'
+        ],
+        [
+            'study_language'=>'HTML',
+            'language_color'=>'54cef9'
+        ],
+        [
+            'study_language'=>'Laravel',
+            'language_color'=>'b29ef3'
+        ],
+        [
+            'study_language'=>'SQL',
+            'language_color'=>'6d46ec'
+        ],
+        [
+            'study_language'=>'SHELL',
+            'language_color'=>'4f45ef'
+        ],
+        [
+            'study_language'=>'情報システム基礎知識(その他)',
+            'language_color'=>'3636c1'
+        ]
+        
+    ];
+    DB::table('languages')->insert($param);
     }
 }

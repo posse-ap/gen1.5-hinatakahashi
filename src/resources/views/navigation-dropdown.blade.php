c19165e2ab4dd673818a39d4bc7465c611a6bd05
junjan-061 <junjan0601@icloud.com>
2022-04-10 16:25:59 +0900

    ApiControllerとTestControllerが必要ないため削除

--- src/resources/views/navigation-dropdown.blade.php
+++ src/resources/views/navigation-dropdown.blade.php
@@ -18,6 +18,8 @@
                 </div>
             </div>
 
+            
+
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-jet-dropdown align="right" width="48">

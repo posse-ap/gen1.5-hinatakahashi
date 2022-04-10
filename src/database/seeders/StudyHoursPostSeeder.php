ae3f7224b877ab2686dbaab3d22e798c17c67068
tomoharuyuyama <tomoharuyuyama@gmail.com>
2022-04-10 16:37:22 +0900

    学習内容のシーダーを今日の日付を使うように変更。

--- src/database/seeders/StudyHoursPostSeeder.php
+++ src/database/seeders/StudyHoursPostSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\StudyHoursPost;
+use Carbon\Carbon;
 
 class StudyHoursPostSeeder extends Seeder
 {
@@ -14,24 +15,25 @@ class StudyHoursPostSeeder extends Seeder
      */
     public function run()
     {
+        $dt = new Carbon(date('Y-m-d'));
         StudyHoursPost::truncate();
 
         StudyHoursPost::create([
             'user_id' => 2,
             'total_hour' => 2.5,
-            'study_date' => '2020-01-01',
+            'study_date' => $dt->addDay(),
         ]);
 
         StudyHoursPost::create([
             'user_id' => 1,
             'total_hour' => 4,
-            'study_date' => '2020-02-02',
+            'study_date' => $dt,
         ]);
 
         StudyHoursPost::create([
             'user_id' => 2,
             'total_hour' => 0.5,
-            'study_date' => '2020-03-03',
+            'study_date' => $dt->subDay(),
         ]);
     }
 }
